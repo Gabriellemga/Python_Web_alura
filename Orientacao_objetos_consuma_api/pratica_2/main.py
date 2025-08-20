186ddafa61d951f70df77bf9078ce39d3e46b42b
Gabrielle <gabriellemga@gmail.com>
2025-08-20 09:35:10 -0300

    Add files via upload

---
+++ Orientacao_objetos_consuma_api/pratica_2/main.py
@@ -0,0 +1,9 @@
+from carro import Carro
+
+carro1 = Carro(marca="Ford", modelo="EcoSport", cor="Preto")
+carro2 = Carro(marca="BYD", modelo="Dolphin", cor="Branco")
+carro3 = Carro(marca="Ferrari", modelo="Roma e Purosangue", cor="Vermelho")
+
+print(f"Carro 1: {carro1.marca} {carro1.modelo}, Cor: {carro1.cor}")
+print(f"Carro 2: {carro2.marca} {carro2.modelo}, Cor: {carro2.cor}")
+print(f"Carro 3: {carro3.marca} {carro3.modelo}, Cor: {carro3.cor}")
